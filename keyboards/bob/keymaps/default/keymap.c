902c922037e8b2ed69d7aea9e732a7d57e6257a3
Arne Wichmann <335090+arnew@users.noreply.github.com>
2024-12-06 08:18:03 +0100

    move Bootloader to F9 to prevent accidents while gaming...

--- keyboards/bob/keymaps/default/keymap.c
+++ keyboards/bob/keymaps/default/keymap.c
@@ -24,7 +24,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_LCTL,KC_LGUI, KC_LALT,KC_SPC,  MO(1), KC_BSPC, KC_SPC, KC_RALT,KC_RGUI,KC_APP,KC_RCTL,KC_LEFT,KC_DOWN,KC_RGHT
     ),
     [1] = LAYOUT(
-QK_BOOT,   _______,   _______,   _______, _______,  KC_MPRV, KC_MPLY, KC_MNXT,     _______,   _______,   _______,   _______, KC_NUM, KC_PSLS, KC_PAST, KC_PMNS,
+   _______,   _______,   _______,   _______, _______,  KC_MPRV, KC_MPLY, KC_MNXT,     _______,   QK_BOOT,   _______,   _______, KC_NUM, KC_PSLS, KC_PAST, KC_PMNS,
    KC_ESC,    KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,   KC_F10,   KC_F11,   KC_F12,   _______,   KC_P7,   KC_P8,   KC_P9,
    _______,   _______,  KC_UP  ,     KC_Y,     KC_U,     KC_I,   _______,   _______,   _______,   _______,   _______,   _______,   _______, KC_PPLS,   KC_P4,   KC_P5,   KC_P6,
    _______,   KC_LEFT, KC_DOWN, KC_RIGHT,     KC_H,     KC_J,  KC_LEFT,   KC_DOWN,   KC_UP, KC_RIGHT,   _______,   _______,   _______,     KC_P1,   KC_P2,   KC_P3,
